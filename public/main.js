dc22ff0de941bac2a96b7f89aeb271fefa44230e
Glitch (glitch-hello-node) <none>
2024-12-21 16:06:38 +0000

    🕷📉 Checkpoint ./public/main.js:1106367/315 ./.gitignore:1106367/5

--- public/main.js
+++ public/main.js
@@ -1,6 +1,5 @@
-// const myServerUrl = 'https://video-support-widget-g2g-glitch.glitch.me/'; // Update this with your backend server URL
-const myServerUrl = 'http://localhost:3000'; // Update this with your backend server URL
-
+const myServerUrl = 'https://video-support-dec2024.glitch.me/';
+// const myServerUrl = 'http://localhost:3000'; // Update this with your backend server URL
 async function getAccessToken() {
     const url = myServerUrl + '/get-access-token';
     const requestOptions = {
